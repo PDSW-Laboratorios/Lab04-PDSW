57be3179329af94433aa486f16441d30fa551fa2
Alejandro Anzola <anzola95avila@gmail.com>
2017-02-15 22:13:01 -0500

    implementacion preliminar clase java

--- GuessTheNumber/src/main/java/com/eci_pdsw/guessthenumber/beanEstadoAdivinanza.java
+++ GuessTheNumber/src/main/java/com/eci_pdsw/guessthenumber/beanEstadoAdivinanza.java
@@ -13,23 +13,62 @@ import javax.enterprise.context.SessionScoped;
  *
  * @author fabian
  */
-@ManagedBean(name = "beanEstadoAdivinanza")
+@ManagedBean(name="beanEstadoAdivinanza")
 @SessionScoped
 public class beanEstadoAdivinanza implements Serializable{
     
+    private static final int INITIAL_PRIZE = 100000;
+    private static final int MINIMUM = 0;
+    private static final int MAXIMUM = 100;
+    private static final int STEPS = 10000;
+    private static final String status1 = "Sigue intentando, te falta poco para lograrlo.";
+    private static final String status2 = "¡Ganaste! Tu premio es de ";
+    
+    
+    private boolean showSend=true;
+    private boolean showReset=true;
     private int number;
-    private int maximum = 100;
     private int attempts;
     private int prize;
-    private int iniPrize = 100000;
     private String gameStatus;
-    private String status1 = "Sigue intentando, te falta poco para lograrlo.";
-    private String status2 = "¡Ganaste! Tu premio es de ";
+    
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public int getAttempts() {
+        return attempts;
+    }
+    
+    public int getPrize() {
+        return prize;
+    }
+    
+    public String getStatus() {
+        return gameStatus;
+    }
+    
+    private int bounds(int value, int min, int max) {
+        return Math.max(Math.min(value, max), min);
+    }
+    
+    public void setGuessNumber(int guessedNumber) {
+        if (prize > MINIMUM) {
+            if (guessedNumber == number) {
+                gameStatus = status2 + prize;
+            } else {
+                gameStatus = status1;
+                prize = bounds(prize - STEPS, MINIMUM, MAXIMUM);
+                attempts++;
+            }
+        }
+    }
     
     public void reiniciar(){
-        number = (int) (Math.random()*maximum);
+        number = (int) (Math.random() * MAXIMUM);
         attempts = 0;
-        prize = iniPrize;
+        prize = INITIAL_PRIZE;
         gameStatus = status1;
     }
 }
